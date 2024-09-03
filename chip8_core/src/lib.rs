817339f331ae4ce023e349b0ee47c2c5f33faa11
Erwin Mintiens <erwin.mintiens@protonmail.com>
2024-09-03 18:28:23 +0200

    Implemented extra opcodes and basic tests

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -58,6 +58,22 @@ impl Emulator {
         emu
     }
 
+    /// Set a v register to a specific value
+    fn set_v_reg(&mut self, v_reg: usize, value: u8) {
+        if v_reg > 15 {
+            panic!(
+                "v register value {} not accessible. Must be a value from 0 to 15.",
+                v_reg
+            );
+        }
+        self.v_reg[v_reg as usize] = value;
+    }
+
+    /// Clear the screen: set all list values to false
+    fn clear_screen(&mut self) {
+        self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
+    }
+
     /// Push a value to the stack and increase the stack_pointer with 1
     fn push(&mut self, value: u16) {
         self.stack[self.stack_pointer as usize] = value;
@@ -101,6 +117,10 @@ impl Emulator {
         operation_code
     }
 
+    /// Execute the given operation code.
+    ///
+    /// This function transforms the given 16 bit opcode to 4 hexadecimal digits and matches the
+    /// values to execute the expected operation.
     fn execute_opcode(&mut self, opcode: u16) {
         let digit1 = (opcode & 0xF000) >> 12;
         let digit2 = (opcode & 0x0F00) >> 8;
@@ -108,23 +128,93 @@ impl Emulator {
         let digit4 = opcode & 0x000F;
 
         match (digit1, digit2, digit3, digit4) {
-            (0, 0, 0, 0) => return, // "NOP"; Do nothing
+            // "NOP"; Do nothing
+            (0, 0, 0, 0) => return,
+            // Clear screen
             (0, 0, 0xE, 0) => {
-                self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
-            } // Clear screen
+                self.clear_screen();
+            }
+            // Return from subroutine
             (0, 0, 0xE, 0xE) => {
                 self.program_counter = self.pop();
-            } // Return from subroutine
+            }
+            // Jump to
             (1, _, _, _) => {
                 let nnn = opcode & 0xFFF;
                 self.program_counter = nnn;
-            } // Jump to
+            }
+            // Call subroutine
             (2, _, _, _) => {
                 let nnn = opcode & 0xFFF;
                 self.push(self.program_counter);
                 self.program_counter = nnn;
-            } // Call subroutine
-            (_, _, _, _) => unimplemented!("Unimplemented operation code: {}", opcode),
+            }
+            // Skip if VX == 0xNN
+            (3, _, _, _) => {
+                let x = digit2 as usize;
+                let nn = (opcode & 0xFF) as u8;
+                if self.v_reg[x] == nn {
+                    self.program_counter += 2;
+                }
+            }
+            // Skip if VX != 0xNN
+            (4, _, _, _) => {
+                let x = digit2 as usize;
+                let nn = (opcode & 0xFF) as u8;
+                if self.v_reg[x] != nn {
+                    self.program_counter += 2;
+                }
+            }
+            // Skip if VX == VY
+            (5, _, _, 0) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                if self.v_reg[x] == self.v_reg[y] {
+                    self.program_counter += 2;
+                }
+            }
+
+            (6, _, _, _) => {
+                let x = digit2 as usize;
+                let nn = (opcode & 0xFF) as u8;
+                self.set_v_reg(x, nn);
+            }
+
+            /*
+            TODO:
+            7XNN 	VX += 0xNN
+            8XY0 	VX = VY
+            8XY1 	VX |= VY
+            8XY2 	VX &= VY
+            8XY3 	VX ^= VY
+            8XY4 	VX += VY
+            8XY5 	VX -= VY
+            8XY6 	VX >>= 1
+            8XY7 	VX = VY - VX
+            8XYE 	VX <<= 1
+            9XY0 	Skip if VX != VY
+            ANNN 	I = 0xNNN
+            BNNN 	Jump to V0 + 0xNNN
+            CXNN 	VX = rand() & 0xNN
+            DXYN 	Draw sprite at (VX, VY)
+            EX9E 	Skip if key index in VX is pressed
+            EXA1 	Skip if key index in VX isn't pressed
+            FX07 	VX = Delay Timer
+            FX0A 	Waits for key press, stores index in VX
+            FX15 	Delay Timer = VX
+            FX18 	Sound Timer = VX
+            FX1E 	I += VX
+            FX29 	Set I to address of font character in VX
+            FX33 	Stores BCD encoding of VX into I
+            FX55 	Stores V0 thru VX into RAM address starting at I
+            FX65 	Fills V0 thru VX with RAM values starting at address in I
+            */
+            // In case opcode doesn't match, panic the program
+            (_, _, _, _) => unimplemented!(
+                "Unimplemented operation code: '{}' (hex='{}')",
+                opcode,
+                format!("{}{}{}{}", digit1, digit2, digit3, digit4)
+            ),
         }
     }
 
@@ -141,6 +231,131 @@ impl Emulator {
             _ => {
                 self.sound_timer -= 1;
             }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// Test setting a v register to a value using the dedicated method
+    #[test]
+    fn set_v_reg() {
+        let mut emul = Emulator::new();
+        emul.set_v_reg(12, 0x0089);
+        assert_eq!(emul.v_reg[12], 0x0089);
+    }
+
+    /// Test setting a non existing v register to a value using the dedicated method, which should panic
+    #[test]
+    #[should_panic]
+    fn set_v_reg_invalid() {
+        let mut emul = Emulator::new();
+        emul.set_v_reg(16, 0x0089);
+    }
+
+    mod test_opcode_execution {
+        use super::*;
+
+        /// Test non implemented opcode. This execution should panic.
+        #[test]
+        #[should_panic]
+        fn non_implemented_opcode() {
+            let mut emul = Emulator::new();
+            emul.execute_opcode(0x0011)
+        }
+
+        /// Test do nothing opcode
+        #[test]
+        fn opcode_0000() {
+            let mut emul = Emulator::new();
+            emul.execute_opcode(0x0000);
+        }
+
+        /// Test clearing screen opcode
+        #[test]
+        fn opcode_00e0() {
+            let mut emul = Emulator::new();
+            emul.screen = [true; SCREEN_WIDTH * SCREEN_HEIGHT];
+            emul.execute_opcode(0x00E0);
+            assert_eq!(emul.screen, [false; SCREEN_WIDTH * SCREEN_HEIGHT]);
+        }
+
+        /// Test return from subroutine
+        #[test]
+        fn opcode_00ee() {
+            let mut emul = Emulator::new();
+            emul.stack[1] = 0x0011;
+            emul.stack_pointer = 2;
+            emul.execute_opcode(0x00EE);
+            assert_eq!(emul.stack_pointer, 1);
+            assert_eq!(emul.program_counter, 0x0011);
+        }
+
+        /// Test jump to
+        #[test]
+        fn opcode_1nnn() {
+            let mut emul = Emulator::new();
+            emul.execute_opcode(0x1234);
+            assert_eq!(emul.program_counter, 0x234);
+        }
+
+        /// Test call subroutine
+        #[test]
+        fn opcode_2nnn() {
+            let mut emul = Emulator::new();
+            emul.program_counter = 123;
+            emul.execute_opcode(0x2345);
+            assert_eq!(emul.program_counter, 0x345);
+            assert_eq!(emul.stack[0], 123);
+            assert_eq!(emul.stack_pointer, 1);
+        }
+
+        /// Test skip if VX == 0xNN
+        #[test]
+        fn opcode_3nnn() {
+            let mut emul = Emulator::new();
+            emul.program_counter = 0;
+            emul.v_reg[6] = 0x0078;
+            emul.execute_opcode(0x3678);
+            assert_eq!(emul.program_counter, 2); //v6 matches 0x0078, so program counter should increase by 2
+            emul.execute_opcode(0x3689);
+            assert_eq!(emul.program_counter, 2); //v6 does not match 0x0078, so program counter should not increase by 2
+        }
+
+        /// Test skip if VX != 0xNN
+        #[test]
+        fn opcode_4nnn() {
+            let mut emul = Emulator::new();
+            emul.program_counter = 0;
+            emul.set_v_reg(11, 0x0033);
+            emul.execute_opcode(0x4B33);
+            assert_eq!(emul.program_counter, 0); // v11 does match 0x0033, program counter should not increase
+            emul.execute_opcode(0x4B01);
+            assert_eq!(emul.program_counter, 2); // v11 does not match 0x0001, program counter should increase with 2
+        }
+
+        /// Test skip if VX == VY
+        #[test]
+        fn opcode_5xy0() {
+            let mut emul = Emulator::new();
+            emul.program_counter = 0;
+            emul.set_v_reg(8, 0x0093);
+            emul.set_v_reg(15, 0x0093);
+            emul.execute_opcode(0x58F0);
+            assert_eq!(emul.program_counter, 2); // v8 matches v15 => program counter should increase with 2
+            emul.set_v_reg(15, 0x0025);
+            emul.execute_opcode(0x58F0);
+            assert_eq!(emul.program_counter, 2); // v8 does not match v15 => program counter should stay the same
+        }
+
+        /// Test set VX to NN
+        #[test]
+        fn opcode_6xnn() {
+            let mut emul = Emulator::new();
+            emul.execute_opcode(0x6722);
+            assert_eq!(emul.v_reg[7], 0x0022);
         }
     }
 }
